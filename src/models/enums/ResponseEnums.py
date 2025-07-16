0199c92c5fe16a3df6a038df78bec0ab8cedb26d
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-07-16 12:42:00 +0300

    add PROCESSING signals

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -8,3 +8,6 @@ class ResponseSignal(Enum):
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
     FILE_UPLOAD_SUCCESS = "file_uploaded_successfully"
     FILE_UPLOAD_FAILED = "file_upload_failed"
+
+    PROCESSING_SUCCESS = "processing_success"
+    PROCESSING_FAILED = "processing_failed"
