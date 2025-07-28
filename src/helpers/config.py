2acf83293f37a67bd3a8be84abd9d1568c7872f1
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-07-28 16:25:20 +0300

    add LANG

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
 
     APP_NAME: str
     APP_VERSION: str
-    OPENAI_API_KEY: str
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
@@ -14,7 +13,7 @@ class Settings(BaseSettings):
     MONGODB_URL: str
     MONGODB_DATABASE: str
 
-    GENERATION_BACKEND = str
+    GENERATION_BACKEND: str
     EMBEDDING_BACKEND: str
 
     OPENAI_API_KEY: str = None
@@ -24,13 +23,16 @@ class Settings(BaseSettings):
     GENERATION_MODEL_ID: str = None
     EMBEDDING_MODEL_ID: str = None
     EMBEDDING_MODEL_SIZE: int = None
-    INPUT_DAFAULT_MAX_CHARACTERS: int = None
-    GENERATION_DAFAULT_MAX_TOKENS: int = None
-    GENERATION_DAFAULT_TEMPERATURE: float = None
+    INPUT_DEFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DEFAULT_MAX_TOKENS: int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
 
     VECTOR_DB_BACKEND: str
     VECTOR_DB_PATH: str
     VECTOR_DB_DISTANCE_METHOD: str = None
+
+    PRIMARY_LANG: str = "en"
+    DEFAULT_LANG: str = "en"
 
     class Config:
         env_file = ".env"
