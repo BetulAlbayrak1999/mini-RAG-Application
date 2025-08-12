5b212611e71ab11ac9753ef3840c32150902a0c7
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-08-12 16:48:40 +0300

    add postgres_conn

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from helpers.config import get_settings, Settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 from stores.llm.templates.template_parser import TemplateParser
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 
 app = FastAPI()
@@ -12,27 +14,26 @@ app = FastAPI()
 
 async def startup_span():
     settings = get_settings()
-    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+    postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+
+    app.db_engine = create_async_engine(postgres_conn)
+    app.db_client = sessionmaker(
+        app.db_engine, class_=AsyncSession, expire_on_commit=False
+    )
 
     llm_provider_factory = LLMProviderFactory(settings)
     vectordb_provider_factory = VectorDBProviderFactory(settings)
 
     # generation client
-    app.generation_client = llm_provider_factory.create(
-        provider=settings.GENERATION_BACKEND
-    )
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
     app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
 
     # embedding client
-    app.embedding_client = llm_provider_factory.create(
-        provider=settings.EMBEDDING_BACKEND
-    )
-    app.embedding_client.set_embedding_model(
-        model_id=settings.EMBEDDING_MODEL_ID,
-        embedding_size=settings.EMBEDDING_MODEL_SIZE,
-    )
-
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+    
     # vector db client
     app.vectordb_client = vectordb_provider_factory.create(
         provider=settings.VECTOR_DB_BACKEND
@@ -46,15 +47,11 @@ async def startup_span():
 
 
 async def shutdown_span():
-    app.mongo_conn.close()
+    app.db_engine.dispose()
     app.vectordb_client.disconnect()
-
 
 app.on_event("startup")(startup_span)
 app.on_event("shutdown")(shutdown_span)
-# app.router.lifespan.on_startup.append(startup_db_client)
-# app.router.lifespan.on_shutdown.append(shutdown_db_client)
-
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
